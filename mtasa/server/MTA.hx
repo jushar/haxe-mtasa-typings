b3070c27102c8740dd602394ca062ad7bd26d386
Paweł Miczka <piorunx@gmail.com>
2017-09-11 01:21:18 +0200

    MTA class cleanup with new methods and documentation

--- mtasa/server/MTA.hx
+++ mtasa/server/MTA.hx
@@ -5,13 +5,41 @@ extern class MTA
 {
   // Global vars
   public static var source: Element;
+  public static var root: Element;
+  public static var resourceRoot: Element;
 
-  // Global functions
+  /**
+    Adds event handler to specific event and source element 
+  **/
   public static function addEventHandler(name: String, attachTo: Element, func: Dynamic): Bool;
+
+  /**
+    Removes event handler for specific event and source element
+  **/
   public static function removeEventHandler(name: String, attachTo: Element, func: Dynamic): Bool;
 
+  /**
+    Creates console with given name
+  **/
+  public static function addCommandHandler(commandName: String, handlerFunction: Player->String->Array<String>->Bool, ?restricted = false, ?caseSensitive = true)
+
+  /**
+    Outputs given message to server log
+  **/
   public static function outputServerLog(message: String): Bool;
+
+  /**
+    Outputs given message to debug
+  **/
   public static function outputDebugString(message: String): Bool;
 
+  /**
+    Outputs given message to ingame console 
+  **/
+  public static function outputConsole(message: String): Bool;
+
+  /**
+    Gets random player from server
+  **/
   public static function getRandomPlayer(): Player;
 }
