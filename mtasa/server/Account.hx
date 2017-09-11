0b8cdd7f8eb422e3a278708105bd1f3c410c7c59
CrosRoad95 <sebajura1234@gmail.com>
2017-09-11 19:31:43 +0200

    new class Account

---
+++ mtasa/server/Account.hx
@@ -0,0 +1,90 @@
+package mtasa.server;
+
+@:native("Account")
+extern class Account{
+    /**
+    This method adds an account to the list of registered accounts of the current server.
+    **/
+    public static function add(name:String, pass:String, ?allowCaseVariations:Bool): Bool;
+
+    /**
+    This method copies all of the data from one account to another.
+    **/
+    public function copyDataTo(account:Account): Bool;
+
+    /**
+    This method retrieves a string that has been stored using setData. Data stored as account data is persistent across user's sessions and maps, unless they are logged into a guest account.
+    **/
+    public function getData(key:String): String;
+
+    /**
+    This variable retrieves the name of an account.
+    **/
+    public var name: String;
+
+    /**
+    This method retrieves the name of an account.
+    **/
+    public function getName(): String;
+
+    /**
+    This method returns the player element that is currently using a specified account, i.e. is logged into it. Only one player can use an account at a time.
+    **/
+    public function getPlayer(): Player;
+
+    /**
+    This variable returns the player element that is currently using a specified account, i.e. is logged into it. Only one player can use an account at a time.
+    **/
+    public var player: Player;
+
+    /**
+    This method returns a table containing all accounts that were logged onto from specified serial. If the serial is empty string, it will return all accounts that were never logged onto.
+    **/
+    public static function getAllBySerial(): Dynamic;
+
+    /**
+    This method returns the last serial that logged onto the specified account.
+    **/
+    public function getSerial(): String;
+
+    /**
+    This variable returns the last serial that logged onto the specified account.
+    **/
+    public var serial: String;
+
+    /**
+    This method returns a table over all the accounts that exist in the server internal.db
+    **/
+    public static function getAll(): Dynamic;
+
+    /**
+    This method returns a table containing all the user data for the account provided
+    **/
+    public function getAllData(): Dynamic;
+
+    /**
+    This method checks to see if an account is a guest account. A guest account is an account automatically created for a user when they join the server and deleted when they quit or login to another account. Data stored in a guest account is not stored after the player has left the server. As a consequence, this method will check if a player is logged in or not.
+    **/
+    public function isGuest(): Bool;
+
+    /**
+    This variable checks to see if an account is a guest account. A guest account is an account automatically created for a user when they join the server and deleted when they quit or login to another account. Data stored in a guest account is not stored after the player has left the server. As a consequence, this variable will check if a player is logged in or not.
+    **/
+    public var guest: Bool;
+
+    /**
+    This method is used to delete existing player accounts.
+    **/
+    public function remove(): Bool;
+
+    /**
+    This method sets a string to be stored in an account. This can then be retrieved using getAccountData. Data stored as account data is persistent across user's sessions and maps, unless they are logged into a guest account. Even if logged into a guest account, account data can be useful as a way to store a reference to your own account system, though it's persistence is equivalent to that of using setElementData on the player's element.
+    **/
+    public function setData(key:String, value:String): Bool;
+
+    /**
+    This function sets the password of the specified account.
+    passwordType: can be "plaintext", "sha256", or "md5"
+    **/
+    public function setPassword(password:String, ?passwordType:String): Bool;
+}
