36d88a288bf7fe0babcf624da089edcbd4ae253a
CrosRoad95 <sebajura1234@gmail.com>
2017-09-13 15:23:29 +0200

    Update Team.hx

--- mtasa/server/Team.hx
+++ mtasa/server/Team.hx
@@ -1,23 +1,22 @@
 package mtasa.server;
 
 import mtasa.shared.Vector3;
+/**
+The team class represents player teams. Players on the same team can use team features such as teamchat or friendly fire.
 
+@see https://wiki.multitheftauto.com/wiki/Element/Team
+**/
 @:native("Team")
 extern class Team{
     /**
-    This function is for creating a new team, which can be used to group players. Players will not join the team until they are respawned.
+    This variable tells you if friendly fire is turned on for the specified team.
     **/
-    public static function create(teamName:String, ?colorR:Int, ?colorG:Int, ?colorB:Int): Object;
+    public static var friendlyFire: Bool;
 
     /**
-    This variable is for returning the number of players in the specified team.
+    This variable retrieves the color of a team.
     **/
-    public static var playerCount: Int;
-
-    /**
-    This method is for returning the number of players in the specified team.
-    **/
-    public function countPlayers(): Int;
+    public static var color: Vector3;
 
     /**
     This variable retrieves all the players of the specified team.
@@ -25,14 +24,29 @@ extern class Team{
     public static var players: Dynamic;
 
     /**
+    This variable is for returning the number of players in the specified team.
+    **/
+    public static var playerCount: Int;
+
+    /**
+    This variable gets the team name of a team object.
+    **/
+    public static var name: String;
+
+    /**
+    This function is for creating a new team, which can be used to group players. Players will not join the team until they are respawned.
+    **/
+    public static function create(teamName:String, ?colorR:Int, ?colorG:Int, ?colorB:Int): Object;
+
+    /**
+    This method is for returning the number of players in the specified team.
+    **/
+    public function countPlayers(): Int;
+
+    /**
     This method retrieves all the players of the specified team.
     **/
     public function getPlayers(): Dynamic;
-
-    /**
-    This variable retrieves the color of a team.
-    **/
-    //public static var color: Vector3;
 
     /**
     This method retrieves the color of a team.
@@ -40,19 +54,9 @@ extern class Team{
     public function getColor(): Vector3;
 
     /**
-    This variable tells you if friendly fire is turned on for the specified team.
-    **/
-    public static var friendlyFire: Bool;
-
-    /**
     This method tells you if friendly fire is turned on for the specified team.
     **/
     public function getFriendlyFire(): Bool;
-
-    /**
-    This variable gets the team name of a team object.
-    **/
-    public static var name: String;
 
     /**
     This method gets the team name of a team object.
@@ -73,5 +77,4 @@ extern class Team{
     This method is used to set a team's name.
     **/
     public function setName(name:String): Bool;
-
 }
