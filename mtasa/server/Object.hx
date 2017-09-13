fb345d8ed36702e17af7857c92a5eb704b7bd3ef
CrosRoad95 <sebajura1234@gmail.com>
2017-09-13 15:48:35 +0200

    Update Object.hx

--- mtasa/server/Object.hx
+++ mtasa/server/Object.hx
@@ -1,16 +1,27 @@
 package mtasa.server;
 
 import mtasa.shared.Vector3;
+/**
+The object class represents static, 3-D models in the GTA world. Objects can only represent models created by the server, they cannot represent models that are part of the GTA's default landscape. Examples of objects include building models, roads, and terrain.
 
+https://wiki.multitheftauto.com/wiki/Element/Object
+**/
 @:native("Object")
 extern class Object extends Element {
-
-  public static function create(model: UInt, position: Vector3, ?rotation: Vector3, ?isLowLOD: Bool): Object;
-
   /**
   This variable returns the visible size of an object.
   **/
   public var scale: Float;
+
+  /**
+  Creates an object in the GTA world.
+  @param  model     a whole integer specifying the GTASA object model ID.
+  @param  position  a vector3 representing the XYZ coordinate on the map.
+  @param  rotation  a vector3 representing the rotation about the XYZ axis in degrees.
+  @param  isLowLod  a bool value specifying if the object will be low LOD. A low LOD object has no collision and a longer draw distance.
+  @see https://wiki.multitheftauto.com/wiki/CreateObject
+  **/
+  public static function create(model: UInt, position: Vector3, ?rotation: Vector3, ?isLowLOD: Bool): Object;
   
   /**
   This method will smoothly move an object from its current position to a specified rotation and position.
