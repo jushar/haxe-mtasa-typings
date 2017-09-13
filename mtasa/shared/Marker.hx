2891f3c36b6baa17463c512b9fc21fb5e7d97a66
CrosRoad95 <sebajura1234@gmail.com>
2017-09-13 15:40:25 +0200

    Update Marker.hx

--- mtasa/shared/Marker.hx
+++ mtasa/shared/Marker.hx
@@ -2,9 +2,29 @@ package mtasa.shared;
 
 import mtasa.server.Element;
 import mtasa.shared.Vector3;
+/**
+The marker class represents colored, 3D shapes in the GTA world. There are several types of markers, including cylinders and checkpoints. In scripts, markers are often used to mark spots and trigger some sort of action when a player goes into them.
 
+@see https://wiki.multitheftauto.com/wiki/Element/Marker
+**/
 @:native("Marker")
 extern class Marker {
+
+    /**
+    This variable returns a float containing the size of the specified marker.
+    **/
+    public var size: Float;
+
+    /**
+    This variable returns the icon name for a marker.
+    **/
+    public var icon: String;
+
+    /**
+    This variable returns the vector3 of the specified marker's target, the vector3  it points to. This only works for checkpoint markers and ring markers. For checkpoints it returns the position the arrow is pointing to, for ring markers it returns the position the ring is facing.
+    **/
+    public var target: Vector3;
+
     /**
     Creates a new file in a directory of a resource. If there already exists a file with the specified name, it is overwritten with an empty file.
     **/
@@ -26,29 +46,14 @@ extern class Marker {
     public function getIcon(): String;
 
     /**
-    This variable returns the icon name for a marker.
-    **/
-    public var icon: String;
-
-    /**
     This method returns a float containing the size of the specified marker.
     **/
     public function getSize(): Float;
-
-    /**
-    This variable returns a float containing the size of the specified marker.
-    **/
-    public var size: Float;
     
     /**
     This method returns the vector3 of the specified marker's target, the vector3 it points to. This only works for checkpoint markers and ring markers. For checkpoints it returns the position the arrow is pointing to, for ring markers it returns the position the ring is facing.
     **/
     public function getTarget(): Vector3;
-
-    /**
-    This variable returns the vector3 of the specified marker's target, the vector3  it points to. This only works for checkpoint markers and ring markers. For checkpoints it returns the position the arrow is pointing to, for ring markers it returns the position the ring is facing.
-    **/
-    public var target: Vector3;
 
     /**
     This method sets the color of the specified marker by modifying the values for red, green and blue.
