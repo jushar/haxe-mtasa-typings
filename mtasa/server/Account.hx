129a5bd5346db4f9ff590c8696d5a7b7ad8d4579
CrosRoad95 <sebajura1234@gmail.com>
2017-09-13 15:30:49 +0200

    Update Account.hx

--- mtasa/server/Account.hx
+++ mtasa/server/Account.hx
@@ -1,7 +1,31 @@
 package mtasa.server;
+/**
+The account class represents a player's server account. You can get the account object associated to any client using getPlayerAccount.
 
+@see https://wiki.multitheftauto.com/wiki/Account
+**/
 @:native("Account")
 extern class Account{
+    /**
+    This variable retrieves the name of an account.
+    **/
+    public var name: String;
+
+    /**
+    This variable returns the player element that is currently using a specified account, i.e. is logged into it. Only one player can use an account at a time.
+    **/
+    public var player: Player;
+
+    /**
+    This variable returns the last serial that logged onto the specified account.
+    **/
+    public var serial: String;
+
+    /**
+    This variable checks to see if an account is a guest account. A guest account is an account automatically created for a user when they join the server and deleted when they quit or login to another account. Data stored in a guest account is not stored after the player has left the server. As a consequence, this variable will check if a player is logged in or not.
+    **/
+    public var guest: Bool;
+
     /**
     This method adds an account to the list of registered accounts of the current server.
     **/
@@ -18,11 +42,6 @@ extern class Account{
     public function getData(key:String): String;
 
     /**
-    This variable retrieves the name of an account.
-    **/
-    public var name: String;
-
-    /**
     This method retrieves the name of an account.
     **/
     public function getName(): String;
@@ -33,11 +52,6 @@ extern class Account{
     public function getPlayer(): Player;
 
     /**
-    This variable returns the player element that is currently using a specified account, i.e. is logged into it. Only one player can use an account at a time.
-    **/
-    public var player: Player;
-
-    /**
     This method returns a table containing all accounts that were logged onto from specified serial. If the serial is empty string, it will return all accounts that were never logged onto.
     **/
     public static function getAllBySerial(): Dynamic;
@@ -46,11 +60,6 @@ extern class Account{
     This method returns the last serial that logged onto the specified account.
     **/
     public function getSerial(): String;
-
-    /**
-    This variable returns the last serial that logged onto the specified account.
-    **/
-    public var serial: String;
 
     /**
     This method returns a table over all the accounts that exist in the server internal.db
@@ -66,11 +75,6 @@ extern class Account{
     This method checks to see if an account is a guest account. A guest account is an account automatically created for a user when they join the server and deleted when they quit or login to another account. Data stored in a guest account is not stored after the player has left the server. As a consequence, this method will check if a player is logged in or not.
     **/
     public function isGuest(): Bool;
-
-    /**
-    This variable checks to see if an account is a guest account. A guest account is an account automatically created for a user when they join the server and deleted when they quit or login to another account. Data stored in a guest account is not stored after the player has left the server. As a consequence, this variable will check if a player is logged in or not.
-    **/
-    public var guest: Bool;
 
     /**
     This method is used to delete existing player accounts.
