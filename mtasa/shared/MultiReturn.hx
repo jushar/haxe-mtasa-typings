8074c3e54a1faa32d27feb270fdad66bf036415b
CrosRoad95 <sebajura1234@gmail.com>
2017-09-14 13:52:16 +0200

    classes for MultiReturn

---
+++ mtasa/shared/MultiReturn.hx
@@ -0,0 +1,20 @@
+package mtasa.shared;
+
+@:multiReturn extern class Color {
+  var r: Int;
+  var g: Int;
+  var b: Int;
+  var a: Int;
+}
+
+@:multiReturn extern class TimerDetails {
+  var timeLeft: Int;
+  var leftExecutes: Int;
+  var ExecutesTimes: Int;
+}
+
+@:multiReturn extern class WaterVertextPosition{
+  var x: Int;
+  var y: Int;
+  var z: Float;
+}
