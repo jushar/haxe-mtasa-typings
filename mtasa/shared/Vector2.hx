ccbea93d2b42a4256ea27485c4f3ca2541526f2d
CrosRoad95 <sebajura1234@gmail.com>
2017-09-15 18:35:36 +0200

    Vector2 class

---
+++ mtasa/shared/Vector2.hx
@@ -0,0 +1,42 @@
+package mtasa.shared;
+
+/**
+The vector2 class representsa two-dimensional vector.
+
+@see https://wiki.multitheftauto.com/wiki/Vector/Vector2
+**/
+@:native("Vector2")
+extern class Vector2 {
+  public var x: Float;
+  public var y: Float;
+
+  public static function create(x: Float, y: Float): Vector2;
+  public function normalize(): Bool;
+
+  /**
+    These functions allow you get y coordinate.
+  **/
+  public function getX(): Float;
+
+  /**
+    These functions allow you get y coordinate.
+  **/
+  public function getY(): Float;
+
+  /**
+    These functions allow you set y coordinate.
+  **/
+  public function setX(x: Float): Float;
+
+  /**
+    These functions allow you set y coordinate.
+  **/
+  public function setY(y: Float): Float;
+  public function getNormalized(): Vector2;
+  public function getSquaredLength(): Float;
+
+  /**
+    Return lenght of vector2.
+  **/
+  public function getLength(): Float;
+}
