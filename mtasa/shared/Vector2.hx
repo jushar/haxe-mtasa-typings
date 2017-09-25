501e17dc29ffc98272f2a466ed6689994dd42f14
Jusonex <jusonex96@gmail.com>
2017-09-25 17:48:00 +0200

    Allow add-operator overloading for Vector2

--- mtasa/shared/Vector2.hx
+++ mtasa/shared/Vector2.hx
@@ -1,17 +1,25 @@
 package mtasa.shared;
 
+abstract Vector2Abstract(Vector2) from Vector2 to Vector2 {
+  @:op(A + B)
+  public function __add(rhs: Vector2): Vector2Abstract {
+    var __add = untyped getmetatable(this);
+    return __add(this, rhs);
+  }
+}
+
 /**
 The vector2 class representsa two-dimensional vector.
 
 @see https://wiki.multitheftauto.com/wiki/Vector/Vector2
 **/
 @:native("Vector2")
-@:op(A+B)
 extern class Vector2 {
   public var x: Float;
   public var y: Float;
 
-  public static function create(x: Float, y: Float): Vector2;
+  public static function create(x: Float, y: Float): Vector2Abstract;
+
   public function normalize(): Bool;
 
   /**
@@ -33,7 +41,7 @@ extern class Vector2 {
     These functions allow you set y coordinate.
   **/
   public function setY(y: Float): Float;
-  public function getNormalized(): Vector2;
+  public function getNormalized(): Vector2Abstract;
   public function getSquaredLength(): Float;
 
   /**
@@ -41,3 +49,4 @@ extern class Vector2 {
   **/
   public function getLength(): Float;
 }
+
