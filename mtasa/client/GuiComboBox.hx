c543821328ea4dc2db003be9e2890cf965dd22ef
StiviiK <stefan.kuerzeder@gmail.com>
2017-09-27 10:48:59 +0200

    Added GuiComboBox

---
+++ mtasa/client/GuiComboBox.hx
@@ -0,0 +1,55 @@
+package mtasa.client;
+
+import mtasa.shared.Vector2;
+
+@:native("GuiComboBox")
+extern class GuiComboBox extends GuiElement {
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiCreateComboBox
+  **/
+  public static function create(position: Vector2, size: Vector2, caption: String, relative: Bool, ?parent: GuiElement): GuiComboBox;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiComboBoxAddItem
+  **/
+  public function addItem(value: String): Int;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiComboBoxClear
+  **/
+  public function clear(): Bool;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiComboBoxRemoveItem
+  **/
+  public function removeItem(itemId: Int): Bool;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiComboBoxGetSelected
+  **/
+  public function getSelected(): Int;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiComboBoxGetItemText
+  **/
+  public function getItemText(itemId: Int): String;
+
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiComboBoxSetItemText
+  **/
+  public function setItemText(itemId: Int, text: String): Bool;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiComboBoxSetSelected
+  **/
+  public function setSelected(itemIndex: Int): Bool;
+}
