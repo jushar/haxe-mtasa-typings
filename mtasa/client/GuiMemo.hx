b350e8f21e8ff8fc2fa621cfd1361d45da202973
StiviiK <stefan.kuerzeder@gmail.com>
2017-09-27 10:39:22 +0200

    Added GuiMemo

---
+++ mtasa/client/GuiMemo.hx
@@ -0,0 +1,30 @@
+package mtasa.client;
+
+import mtasa.shared.Vector2;
+
+@:native("GuiMemo")
+extern class GuiMemo extends GuiElement {
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiCreateMemo
+  **/
+  public static function create(position: Vector2, size: Vector2, text: String, relative: Bool, parent: GuiElement): GuiMemo;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiMemoGetCaretIndex
+  **/
+  public function getCaretIndex(): Int;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiMemoSetCaretIndex
+  **/
+  public function setCaretIndex(index: Int): Bool;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiMemoSetReadOnly
+  **/
+  public function setReadOnly(status: Bool): Bool;
+}
