ce8286b8d9b573bbc2195d27c3f4cfd8894b3c9c
StiviiK <stefan.kuerzeder@gmail.com>
2017-09-27 10:56:28 +0200

    Added GuiScrollPane

---
+++ mtasa/client/GuiScrollPane.hx
@@ -0,0 +1,42 @@
+package mtasa.client;
+
+import mtasa.shared.Vector2;
+
+@:native("GuiScrollPane")
+extern class GuiScrollPane extends GuiElement {
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiCreateScrollPane
+  **/
+  public static function create(position: Vector2, size: Vector2, relative: Bool, ?parent: GuiElement): GuiScrollPane;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiScrollPaneGetHorizontalScrollPosition
+  **/
+  public function getHorizontalScrollPosition(): Float;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiScrollPaneGetVerticalScrollPosition
+  **/
+  public function getVerticalScrollPosition(): Float;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiScrollPaneSetHorizontalScrollPosition
+  **/
+  public function setHorizontalScrollPosition(position: Float): Bool;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiScrollPaneSetScrollBars
+  **/
+  public function setScrollBars(horizontal: Bool, vertical: Bool): Bool;
+
+  /**
+
+    @see https://wiki.multitheftauto.com/wiki/GuiScrollPaneSetVerticalScrollPosition
+  **/
+  public function setVerticalScrollPosition(position: Float): Bool;
+}
